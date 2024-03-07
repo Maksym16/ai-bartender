9d0f44ae502f14b722f79c23eed5253907292de0
Maksym <mfedorenko16@gmail.com>
2024-03-07 05:51:01 -0500

    project set up

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+const DashboardPage = () => {
+  return (
+    <p className="text-green-500">Dashboard</p>
+  );
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+
+const LandingPage = () => {
+  return (
+    <p className="text-green-500">landing</p>
+  );
+}
+
+export default LandingPage;
