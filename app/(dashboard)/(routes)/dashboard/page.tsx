e8034ecf6350d8f88ba386aae5d5d06b82936c43
Maksym <mfedorenko16@gmail.com>
2024-03-08 04:59:07 -0500

    dashboard and sidebar set up

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from '@clerk/nextjs';
 
 const DashboardPage = () => {
   return (
     <div className="">
-      <p className="text-green-500">Dashboard</p>
-      <UserButton afterSignOutUrl='/' />
+    
     </div>
   );
 };
